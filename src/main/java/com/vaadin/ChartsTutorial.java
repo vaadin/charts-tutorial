6761a8df70dcc353f93d7bf9a0e63534c819568f
Jonatan Kronqvist <jonatan@vaadin.com>
2014-12-10 10:19:56 +0200

    Finalized the Charts tutorial
     - Updated to the latest Vaadin release
     - Updated to the latest Charts release
     - Show two different ways of binding data

--- src/main/java/com/vaadin/ChartsTutorial.java
+++ src/main/java/com/vaadin/ChartsTutorial.java
@@ -1,28 +1,31 @@
 package com.vaadin;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.ChartsData.ShoeSizeInfo;
 import com.vaadin.ChartsData.WeatherInfo;
 import com.vaadin.addon.charts.Chart;
-import com.vaadin.addon.charts.ChartOptions;
 import com.vaadin.addon.charts.model.AxisType;
 import com.vaadin.addon.charts.model.ChartType;
 import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.ContainerDataSeries;
 import com.vaadin.addon.charts.model.DataSeries;
 import com.vaadin.addon.charts.model.DataSeriesItem;
-import com.vaadin.addon.charts.model.Options3d;
 import com.vaadin.addon.charts.model.PlotOptionsColumn;
 import com.vaadin.addon.charts.model.PlotOptionsLine;
 import com.vaadin.addon.charts.model.YAxis;
 import com.vaadin.addon.charts.model.style.SolidColor;
-import com.vaadin.addon.charts.themes.VaadinLightTheme;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Container.Filter;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.filter.Compare.Equal;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.Date;
 
 @Theme("mytheme")
 @SuppressWarnings("serial")
@@ -39,8 +42,6 @@ public class ChartsTutorial extends UI
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
-
-        ChartOptions.get().setTheme(new VaadinLightTheme());
 
         ChartsData data = new ChartsData();
         layout.addComponent(getShoeSizeChart(data));
@@ -90,27 +91,38 @@ public class ChartsTutorial extends UI
 
         conf.getChart().setType(ChartType.LINE);
 
-        DataSeries temp = new DataSeries("Temperature");
-        for (int i=0; i<data.getWeatherData().size(); i+=14) {
-            WeatherInfo weatherInfo = data.getWeatherData().get(i);
-            // Date on the X-axis, mean temperature on the Y-axis
-            temp.add(new DataSeriesItem(
-                    weatherInfo.getDate(),
-                    weatherInfo.getMaxTemp()));
-        }
+        BeanItemContainer<WeatherInfo> weatherContainer =
+                new BeanItemContainer<WeatherInfo>(
+                        WeatherInfo.class, data.getWeatherData());
+
+        ContainerDataSeries temp = new ContainerDataSeries(weatherContainer);
+        temp.setName("Temperature");
+        temp.setXPropertyId("date");    // will call WeatherInfo.getDate()
+        temp.setYPropertyId("maxTemp"); // will call WeatherInfo.getMaxTemp()
+
         conf.getxAxis().setTitle("Date");
         conf.getxAxis().setType(AxisType.DATETIME);
         conf.getyAxis().setTitle("Temperature (°C)");
 
-        DataSeries humidity = new DataSeries("Humidity");
-        for (int i=0; i<data.getWeatherData().size(); i+=14) {
-            WeatherInfo weatherInfo = data.getWeatherData().get(i);
-            // Date on the X-axis, humidity on the Y-axis
-            humidity.add(new DataSeriesItem(
-                    weatherInfo.getDate(),
-                    weatherInfo.getMeanHumidity()));
-        }
+        ContainerDataSeries humidity = new ContainerDataSeries(weatherContainer);
+        humidity.setName("Humidity");
+        humidity.setXPropertyId("date");            // will call WeatherInfo.getDate()
+        humidity.setYPropertyId("meanHumidity");    // will call WeatherInfo.getMeanHumidity()
         humidity.setPlotOptions(new PlotOptionsColumn());
+
+        weatherContainer.addContainerFilter(new Filter() {
+            @Override
+            public boolean passesFilter(Object o, Item item) throws UnsupportedOperationException {
+                Date date = (Date)item.getItemProperty("date").getValue();
+                return date.getDay() == 0;
+            }
+
+            @Override
+            public boolean appliesToProperty(Object o) {
+                return "date".equals(o);
+            }
+        });
+
         conf.addSeries(humidity);
         conf.addSeries(temp);
 

97227c8401c3db36537fcc97d2d6d142aabd637f
Dmitrii Rogozin <dmitrii@vaadin.com>
2016-03-10 18:24:26 +0200

    Add declarative tutorial

---
+++ declarative/src/main/java/com/vaadin/ChartsTutorial.java
@@ -0,0 +1,27 @@
+package com.vaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+import javax.servlet.annotation.WebServlet;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class ChartsTutorial extends UI
+{
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = ChartsTutorial.class, widgetset = "com.vaadin.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        DeclarativeBasicLine line= new DeclarativeBasicLine();
+        setContent(line);
+    }
+
+}
